3585cc53737a76df0b20ca0c62a87b5cb56b3f31
Aayush Bhasin <Water292@users.noreply.github.com>
2016-10-08 10:33:50 -0400

    Update twitterstream.py
    streaming code that can be executed by server

--- twitterstream.py
+++ twitterstream.py
@@ -5,7 +5,6 @@ import time
 import sys
 import os
 import datetime
-import socket
 from pprint import pprint
 
 db_client = MongoClient()
@@ -20,21 +19,20 @@ def collection(collection_name):
     return db_object[str(collection_name)]
 
 i = 0
-endTime = time.time() + 1
-
+endTime = int(sys.argv[1])/1000
 
 class StreamListener(tweepy.StreamListener):
-
     def on_status(self, tweet):
-        if time.time() > endTime: 
+        if time.time() > endTime:
             print("Over")
             return False
+
         type_str = ""
         tweet_reply_id=""
         tweet_reply_user=""
-        
-        if (not (tweet.retweeted or tweet.in_reply_to_screen_name or 
-    	    tweet.in_reply_to_status_id or tweet.in_reply_to_user_id)): 
+
+        if (not (tweet.retweeted or tweet.in_reply_to_screen_name or
+    	    tweet.in_reply_to_status_id or tweet.in_reply_to_user_id)):
             type_str = "original"
         elif (tweet.retweeted):
             type_str = "retweet"
@@ -44,16 +42,13 @@ class StreamListener(tweepy.StreamListener):
     	    type_str = "reply"
     	    tweet_reply_id=tweet.in_reply_to_status_id
     	    tweet_reply_user=tweet.in_reply_to_user_id
-  
-        tempkeywords = []
-        tempchannels = []
 
         db_object.messages.insert_one({
             'user_id' : tweet.user.id_str,
             'tweet_id': tweet.id_str,
             'text' : tweet.text,
-            'channels' : ['PythonTest'],
-            'keywords' : ['Donald', 'Trump', 'Hillary', 'Clinton'],
+            'channels' : [sys.argv[2]],
+            'keywords' : sys.argv[3:],
             'type': type_str,
             'in_reply_to_tweet_id': tweet_reply_id,
             'in_reply_to_user_id': tweet_reply_user,
@@ -63,36 +58,37 @@ class StreamListener(tweepy.StreamListener):
             'timeCollected': datetime.datetime.now()
         })
 
-        global i 
+        global i
         i += 1
         # if i % 1000 == 0: print(i)
-        print(i)
+        # print(i)
 
     def on_error(self, status_code):
         raise tweepy.error.TweepError(status_code)
 
+
 class ErrorHandler(object):
     """Handles errors raised by the Twitter Streaming API.
- 
+
 This class contains methods to handle various types of errors that
 may occur while connecting to the Streaming API. Each method
 sleeps, logs the error, and increments the error count. The sleep
 durations and error count are reset if the last error occurred more
 than 60 minutes ago.
- 
+
 See also: https://dev.twitter.com/docs/streaming-apis/connecting
- 
+
 Attributes:
 sleep_{rate_limit, http_error, network_error, db_error}:
 Seconds to sleep before retrying the API connection.
 error_count: Number of errors that have occurred.
 time_of_last_error: The Unix time when the last error occurred.
 """
- 
+
     def __init__(self):
         """Initializes class with default parameter values."""
         self._reset()
- 
+
     def _reset(self):
         """Reset class parameters to default values."""
         self.sleep_rate_limit = 60
@@ -101,7 +97,7 @@ time_of_last_error: The Unix time when the last error occurred.
         self.sleep_db_error = .25
         self.error_count = 0
         self.time_of_last_error = time.time()
- 
+
     def _decorator(func):
         """Reset (if necessary) and increment class parameters."""
         def wrapper(self, *args):
@@ -111,35 +107,37 @@ time_of_last_error: The Unix time when the last error occurred.
             self.error_count += 1
             self.time_of_last_error = time.time()
         return wrapper
- 
+
     @_decorator
     def rate_limit(self, status_code):
         msg = 'HTTP error, status code {}. Retrying in {} seconds.'
         msg = msg.format(status_code, self.sleep_rate_limit)
         time.sleep(self.sleep_rate_limit)
         self.sleep_rate_limit *= 2
- 
+
     @_decorator
     def http_error(self, status_code):
         msg = 'HTTP error, status code {}. Retrying in {} seconds.'
         msg = msg.format(status_code, self.sleep_http_error)
         time.sleep(self.sleep_http_error)
         self.sleep_http_error = min(self.sleep_http_error * 2, 320)
- 
+
     @_decorator
     def network_error(self):
         msg = 'TCP/IP error. Retrying in {} seconds.'
         msg = msg.format(self.sleep_network_error)
         time.sleep(self.sleep_network_error)
         self.sleep_network_error = min(self.sleep_network_error + 1, 16)
- 
+
     @_decorator
     def db_error(self):
         msg = 'Database error. Retrying in {} seconds.'
         msg = msg.format(self.sleep_db_error)
         time.sleep(self.sleep_db_error) # Sleep time remains constant.
 
+
 if __name__ == '__main__':
+    print(sys.argv)
     l = StreamListener()
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -149,16 +147,15 @@ if __name__ == '__main__':
             print("here")
             try:
                 print("try here")
-                stream = tweepy.Stream(auth, l, 
+                stream = tweepy.Stream(auth, l,
                     wait_on_rate_limit_notify = True)
-                stream.filter(track=['Trump', 'Clinton'])
-            except KeyboardInterrupt: 
-                break
+                stream.filter(track=sys.argv[3:])
+            except KeyboardInterrupt: break
             except Exception as e:
                 print(e)
                 if error_handler.error_count > 10:
                     break
-                if (isinstance(e, tweepy.error.TweepError) and 
+                if (isinstance(e, tweepy.error.TweepError) and
                     (e.args[0] == 420 or e.args[0] >= 500)):
                     error_handler.rate_limit(e.args[0])
                 elif isinstance(e, tweepy.error.TweepError):
